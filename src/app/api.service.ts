ff2475f486aba7768378f8f05033eec033557730
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-08-07 18:14:34 +0530

    7 aug 2023
    0614pm

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-  
+import { Observable } from 'rxjs';
+import { User } from './user.model';
 @Injectable({
     providedIn: 'root'
 })
 export class ApiService {
     constructor(private http: HttpClient) { }
-    getMessage() {
+    /*getMessage() {
         return this.http.get(
             'http://localhost:3000/api/message');
+    }*/
+
+    getUserdetail(){
+        return this.http.get(
+            'http://localhost:3000/users');
+
+            /*return this.http.get('http://localhost:3000/users')
+                    .toPromise()
+                    .then(res => res as Product[]);*/
+    }
+
+    getAll() :Observable<User[]>{
+        return this.http.get<User[]>(
+            'http://localhost:3000/users');
+
     }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './api.service';
+import { User } from './user.model';
+ 
+
 
 @Component({
   selector: 'app-root',
@@ -9,13 +12,29 @@ import { ApiService } from './api.service';
 export class AppComponent implements OnInit{
   title = 'express_example';
   message :any;
+  //displaydata : User[]= [];
 
+  displaydata: User[];
+  
   constructor(private apiService : ApiService){
 
   }
   ngOnInit(){
-    this.apiService.getMessage().subscribe(data => {
+    /*this.apiService.getMessage().subscribe(data => {
       this.message = data;
-    });
+    });*/
+
+    /*this.apiService.getUserdetail().subscribe((data) => {
+     console.log(data);
+      //this.displaydata = data;
+    })*/
+
+    this.apiService.getAll().subscribe(data => {
+      this.displaydata = data;
+      console.log(data);
+      },
+      error => {
+        console.log(error);
+      })
   }
 }
